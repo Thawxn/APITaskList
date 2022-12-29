abef76d35de77ae428796d954b5b54d12c4ad796
Thawan Marques <thawan28032001@gmail.com>
2022-12-29 18:54:41 -0300

    User registration

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'Thawan',
-    email: 'thawan@thawan.com',
-    password_hash: '12345',
-  });
-
-  return res.json(user);
-});
+routes.post('/user', UserController.store);
 
 export default routes;
